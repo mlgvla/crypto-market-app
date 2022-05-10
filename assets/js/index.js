7618990a33ba9ffbbd897602da75ab3d684df8e9
NickeaBennett <nickeabennett@gmail.com>
2022-05-10 07:55:49 -0400

    Created function to fetch coins

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,30 @@
-function fetchCoin() {
-   return fetch("https://api.coingecko.com/api/v3/coins/list", {
+// Function to fetch API endpoint. Converted from Curl to Fetch.
+function trendingCoins() {
+    return fetch("https://api.coingecko.com/api/v3/search/trending", {
+        headers: {
+        Accept: "application/json"
+        }
+    })
+        .then(resp => resp.json())
+        .then(json => renderCoins(json));
+ }
+
+function fetchCoins() {
+   return fetch("https://api.coingecko.com/api/v3/search?query= ", { //check the end of query
     headers: {
       Accept: "application/json"
     }
   })
+        .then(resp => resp.json())
+        .then(json => renderCoins(json));
 }
-fetchCoin()
+
+function renderCoins() {
+    
+}
+
+ 
+// Fetch API on page load. 
+document.addEventListener("DOMContentLoaded", function() {
+    trendingCoins()
+});
