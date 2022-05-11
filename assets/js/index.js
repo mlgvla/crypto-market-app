f2b1500c97a350655758d3297ad858738664ef0c
NickeaBennett <nickeabennett@gmail.com>
2022-05-11 14:06:34 -0400

    Top and main search function update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,67 +1,88 @@
 // Function to fetch API endpoint. Converted from Curl to Fetch.
-function trendingCoins() {
+function fetchTrendingCoins() {
     return fetch("https://api.coingecko.com/api/v3/search/trending", {
         headers: {
             Accept: "application/json"
         }
     })
     .then(resp => resp.json())
-    .then(json => renderCoins(json.coins));
+    .then(json => renderTrendingCoins(json.coins));
 }
 
-// Function to search for coins based on user input.
-const init = () => {
-    const searchForm = document.querySelector('form');
-
-    searchForm.addEventListener('submit', (event) => {
+// Top search Function to search for coins based on user input.
+const topSearch = () => {
+    const topSearchForm = document.querySelector('#form-1');
+    topSearchForm.addEventListener('submit', (event) => {
         event.preventDefault()
         const input = document.querySelector('input#searchCoinID');
+  
+        fetch(`https://api.coingecko.com/api/v3/search?query=${input.value}`, { 
 
-        fetch(`https://api.coingecko.com/api/v3/search?query=${input.value}`)
-            .then(resp => resp.json()) 
-            .then(json => {
-                renderCoins(json.coins)
-                console.log(json)
-            })
+                 headers: {
+                    Accept: "application/json"
+                 }
+             })
+                .then( response => response.json())
+                .then(json => {
+        
+                    renderTrendingCoins(json.coins)
+                })
     });
 }
-// function searchCoins() {
-//    return fetch("https://api.coingecko.com/api/v3/search?query= ", { //check the end of query
-//         headers: {
-//             Accept: "application/json"
-//         }
-//     })
-//         .then(resp => resp.json())
-//         .then(json => renderCoins(json));
-// }
+// Top search Function END!
+// Main search function to search for coins based on user input.
+const mainSearch = () => {
+    const mainSearchForm = document.querySelector('#form-2');
+    mainSearchForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const input = document.querySelector('input#searchCoinID2');
+  
+        fetch(`https://api.coingecko.com/api/v3/search?query=${input.value}`, { 
 
+                 headers: {
+                    Accept: "application/json"
+                 }
+             })
+                .then( response => response.json())
+                .then(json => {
+        
+                    renderTrendingCoins(json.coins)
+                })
+    });
+}
+// Main search Function END!
+// For search forms
+document.addEventListener('DOMContentLoaded', topSearch);
+document.addEventListener('DOMContentLoaded', mainSearch);
 
-function renderCoins(coins) {
+// Function to render top 7 trending coins on the DOM.
+function renderTrendingCoins(coins) {
     console.log(coins)
-    const cyrptoName = document.querySelector('.list-group');
+    const listGroup = document.querySelector('.list-group');
     coins.forEach(coin => {
         const div = document.createElement('div');
         const results = document.querySelector('#results');
         div.className = 'list-group';
         div.innerHTML = `
         <a href="#" class="list-group-item list-group-item-action">
+                <img src="${coin.item.small}">
             <div class="d-flex w-100 justify-content-between">
                 <div id="crypto-name">
-                    <h5 class="mb-1">${coin.item.name}</h5>
+                    <h6 class="mb-1">${coin.item.name} (${coin.item.symbol})</h6>
                 </div>
-                <small>Market Cap Rank:</small>
+                <small>Market Cap Rank: ${coin.item.market_cap_rank}</small>
             </div>
-            <p class="mb-1">Some placeholder content in a paragraph.</p>
-            <small>And some small print.</small>
+            <p class="mb-1">Price: ${coin.item.price_btc}</p>
+            <small>Score: ${coin.item.score}</small>
         </a>
         `
         // div.textContent = <small>And some small print.</small>
-        cyrptoName.append(div);
+        listGroup.append(div);
         
     });
 }
  
 // Call trendingCoins function and fetch API on page load. 
 document.addEventListener("DOMContentLoaded", function() {
-    trendingCoins()
+    fetchTrendingCoins()
 });
