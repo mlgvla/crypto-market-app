1412768faa3066891f374207e591c0ce601f0b9c
NickeaBennett <nickeabennett@gmail.com>
2022-05-19 15:12:23 -0400

    Completed project requirements

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,7 +31,7 @@ function renderApiData(json) {
         <button data-symbol="${coin.symbol}" id="modalButton" type="button" class="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#modalID">
                 <img id="searchCoinImage" src="${coin.image}" alt="" height="30px">
             <div class="d-flex w-100 justify-content-between">
-                    <h6 id="coinName" class="mb-1">${coin.name} (${coin.symbol})</h6>
+            <h6 id="coinName" class="mb-1">${coin.name} (${coin.symbol})</h6>
                 <small>Market Rank: <span class="marketRankID">${coin.market_cap_rank}</span></small>
             </div>
             <small>Current Price: $${coin.current_price}</small>
@@ -44,7 +44,6 @@ function renderApiData(json) {
         // onclick="modalData()"
     });
 }
-
 
 /*
 Top search Function to search for coins based on user input. 
@@ -81,9 +80,8 @@ const mainSearch = () => {
     const mainSearchForm = document.querySelector('#form-2');
     mainSearchForm.addEventListener('submit', (event) => {
         event.preventDefault()
-        const input = document.querySelector('input#searchCoinID2');
-  
-        fetch(`https://api.coingecko.com/api/v3/search?query=${input.value}`, { 
+        const userInput = document.querySelector('input#searchCoinID2');
+        fetch(`https://api.coingecko.com/api/v3/search?query=${userInput.value}`, { 
 
                  headers: {
                     Accept: "application/json"
@@ -94,6 +92,7 @@ const mainSearch = () => {
                     renderSearchCoins(json.coins)
                 })
     });
+    
 }
 mainSearch()
 // Main search fetch Function END!
@@ -115,9 +114,11 @@ function renderSearchCoins(coins) {
                     </div>
                 </button>
         `
+        div.querySelector('button').addEventListener('click', (e) => {
+            modalData(coin)
+        })
         searchResults.appendChild(div);
     });
-// onclick="modalData()"
 }
 
 function modalData({
@@ -132,30 +133,31 @@ function modalData({
 }) {
     const modalHeader = document.getElementById('modalHeader')
     const modalCoinTitle = document.getElementById('modalH5')
-            modalCoinTitle.textContent = `
-                    ${name} (${symbol})
-                `
+        modalCoinTitle.textContent = `
+            ${name} (${symbol})
+        `
     const modalCoinImage = document.getElementById('modalCoinImage')
-                    modalCoinImage.src = `${image}
-                    `
+        modalCoinImage.src = `
+            ${image}
+        `
     const marketCapRankTD = document.getElementById('marketCapRankTD')
-                    marketCapRankTD.textContent = `
-                        ${market_cap_rank}
-                    `
+        marketCapRankTD.textContent = `
+            ${market_cap_rank}
+        `
     const currentPriceTD = document.getElementById('currentPriceTD')
-                    currentPriceTD.textContent = `
-                        $${current_price}
-                    `
+        currentPriceTD.textContent = `
+            $${current_price}
+        `
     const high24TD = document.getElementById('high24TD')
-                    high24TD.textContent = `
-                        $${high_24h}
-                    `
-                const low24TD = document.getElementById('low24TD')
-                    low24TD.textContent = `
-                        $${low_24h}
-                    `
+        high24TD.textContent = `
+            $${high_24h}
+        `
+    const low24TD = document.getElementById('low24TD')
+        low24TD.textContent = `
+            $${low_24h}
+        `
 }
-         
+
 function fetchTrendingCoins() {
     return fetch("https://api.coingecko.com/api/v3/search/trending", {
         headers: {
@@ -174,7 +176,6 @@ function renderTrendingCoins(coins) {
         const div = document.createElement('div')
         div.className = 'list-group';
         // Must convert FROM innerHTML to textContent
-
         div.innerHTML = `
         <button type="button" class="list-group-item list-group-item-action" data-bs-toggle="modal" data-bs-target="#modalID">
                 <img src="${coin.item.small}" alt="" height="30px">
